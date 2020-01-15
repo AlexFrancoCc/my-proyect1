5ba8b3830f205545986911e8710cced4d706f04f
Alex <alexfrancocc@gmail.com>
2020-01-14 21:55:01 -0500

    Mi primer commit

---
+++ Usarpython.py
@@ -0,0 +1,8 @@
+x=1
+y=3 
+print(x)
+print(y)
+var1=input('Ingrese variable 1: ', )
+var2=input('Ingrese variable 2: ',)
+print('El numero ingresado es:',var1)
+print('El numero ingresado es:',var2)
